d624070a14d25fcda9447777a40d6b7188881d1f
hootbee <xx3xx27@gmail.com>
2025-05-08 00:02:45 +0900

    컴포넌트 쳐내는중

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -23,38 +23,35 @@ const MenuWrapper = styled.div`
   }
 `;
 
-const Hamburger = styled(GiHamburgerMenu)`
-  
-`
+const Hamburger = styled(GiHamburgerMenu)``;
 
 const Menu = () => {
   return (
     <MenuWrapper>
       <a href="/about">
-        <h5>회사 소개</h5>
+        <h5>ABOUT</h5>
       </a>
 
       <a href="/products">
-        <h5>제품 소개</h5>
+        <h5>NEWS</h5>
       </a>
 
       <a href="/stores">
-        <h5>대여점</h5>
+        <h5>MEMBERS</h5>
       </a>
 
       <a href="/news">
-        <h5>뉴스 및 이벤트</h5>
+        <h5>RESEARCH</h5>
       </a>
 
       <a href="/support">
-        <h5>고객 지원</h5>
+        <h5>OPENINGS</h5>
       </a>
 
       <a href="/login">
-        <h5>로그인/회원가입</h5>
+        <h5>CONTACT</h5>
       </a>
     </MenuWrapper>
-    
   );
 };
 
--- src/menu/Home.jsx
+++ src/menu/Home.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import Explain from "../home/Explain";
-import Gallery from "../home/Gallery";
 import Design from "../home/Design";
-import Smarts from "../home/Smarts";
-import Convenience from "../home/Convenience";
-import Safety from "../home/Safety";
-import Specs from "../home/Specs";
+import Final from "../home/Final";
+import Introduce from "../home/Introduce";
 
 const Home = () => {
   return (
     <>
-      <Explain></Explain>
-      <Gallery></Gallery>
+      <Introduce></Introduce>
       <Design></Design>
-      <Smarts></Smarts>
-      <Convenience></Convenience>
-      <Safety></Safety>
-      <Specs></Specs>
+      <Final></Final>
     </>
   );
 };
